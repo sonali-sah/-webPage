0b8e0f4fa9658b6e55f258c0664f469082d50f47
Ritik Kumar Pathak <phritik06@gmail.com>
2024-10-21 10:43:25 +0530

    image fixes

--- vite-project/src/HeroSection.jsx
+++ vite-project/src/HeroSection.jsx
@@ -1,26 +1,28 @@
 const HeroSection = () => {
-    return (
-        <div className="Hero">
-            <div className="hero-content">
-            <h1>YOUR FEET DESERVE THE BEST</h1>
-            <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
-                YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
-                <div className="hero-btn">
-                    <button>Shop Now</button>
-                    <button>Category</button>
-                </div>
-                <div className="shopping"><p>Also Available On</p>
-                <div className="brand-icons">
-                    <img src="/images/amazon.png" alt="amazon-logo" />
-                    <img src="/images/flipkart.png" alt="amazon-logo" />
-                </div>
-                </div>
-                </div>
-            <div className="hero-image">
-            <img src="/images/hero-image.png" alt="hero-image" />
-
-            </div>
+  return (
+    <div className="Hero">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+        </p>
+        <div className="hero-btn">
+          <button>Shop Now</button>
+          <button>Category</button>
         </div>
-    )
+        <div className="shopping">
+          <p>Also Available On</p>
+          <div className="brand-icons">
+            <img src="/image/amazon.png" alt="amazon-logo" />
+            <img src="/image/brand_logo.png" alt="amazon-logo" />
+          </div>
+        </div>
+      </div>
+      <div className="hero-image">
+        <img src="/image/shoe_image.png" alt="hero-image" />
+      </div>
+    </div>
+  );
 };
 export default HeroSection;
