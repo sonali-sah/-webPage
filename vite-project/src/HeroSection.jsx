4b42a7ae6930028f4907ab385ce1f7d1f0f6c751
sonali-sah <Id>
2024-10-21 19:27:01 +0530

    Newchanges

--- vite-project/src/HeroSection.jsx
+++ vite-project/src/HeroSection.jsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
           <p>Also Available On</p>
           <div className="brand-icons">
             <img src="/image/amazon.png" alt="amazon-logo" />
-            <img src="/image/flipkart(2).png" alt="flipkart-logo" />
+            <img src="/image/flipkart.png" alt="flipkart-logo" />
           </div>
         </div>
       </div>
